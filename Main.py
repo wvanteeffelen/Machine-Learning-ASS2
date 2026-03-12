35ef0d78e9dd498012cc90202d9c1cf657cc703c
simondeu <104424674+simondeu@users.noreply.github.com>
2026-03-12 15:07:59 +0100

    Added our own implementation

---
+++ Main.py
@@ -0,0 +1,19 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neighbors import KDTree
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from scipy.spatial import ConvexHull
+from tqdm import tqdm
+from os.path import exists, join
+from os import listdir
+
+def main():
+    pass
+    
+
+
+if __name__=='__main__':
+    main()
